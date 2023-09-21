38a37e94162f4605f4bb1d56ba1c722bd3f4d97c
Alejpg05 <137833794+Alejpg05@users.noreply.github.com>
2023-09-21 18:08:49 -0300

    Cambio de 404

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if ($usuario==$ckusuario & $ckpass==$contrasenia){
 }
 else{
     echo "Acceso denegado";
-    header("location:https://crehana-blog.imgix.net/media/filer_public/08/ee/08eee73d-4589-4bfb-8e2e-b23a29528b73/ejemplos_de_paginas_de_error_404_mcdonalds.jpg?auto=format&q=50");
+    header("location:https://imgs.search.brave.com/wmg6aPtUNSAsYzWTEz8PoDp2qMCdiaTR_o-nUHj7PFg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI5/MDczNDc3Ny92ZWN0/b3IvNDA0LWVycm9y/LXBhZ2UtdmVjdG9y/LWRlc2lnbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9c0Q4/RW5ndlFRSEotblRV/eFRUWkFLY04xYkJh/MlhjbFRUZkYyajQt/V0dpUT0");
 }
 
 ?>
